bc8851dac931a54a386a3e42093c46e77b5a230c
心动随风 <1547812526@qq.com>
2024-10-26 19:36:14 +0800

    Date 2024/10/26: 1.实现后端项目创建 2.实现前端项目创建 3.前端页面模板与后端接口联通

---
+++ MTUE_backend/src/main/java/org/mtue/mtue_backend/MtueBackendApplication.java
@@ -0,0 +1,13 @@
+package org.mtue.mtue_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtueBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtueBackendApplication.class, args);
+    }
+
+}
